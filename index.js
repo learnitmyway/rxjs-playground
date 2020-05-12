987179ec5bfa9a42373ab638979431cc4b6dc1bf
David <learnitmyway@users.noreply.github.com>
2020-05-12 07:41:49 +0200

    npm i rxjs

---
+++ index.js
@@ -0,0 +1,12 @@
+import { fromEvent, interval } from 'rxjs'
+import { concatMap, take } from 'rxjs/operators'
+
+const clicks = fromEvent(document, 'click')
+const result = clicks.pipe(concatMap((ev) => interval(1000).pipe(take(4))))
+result.subscribe((x) => console.log(x))
+
+// Results in the following:
+// (results are not concurrent)
+// For every click on the "document" it will emit values 0 to 3 spaced
+// on a 1000ms interval
+// one click = 1000ms-> 0 -1000ms-> 1 -1000ms-> 2 -1000ms-> 3
