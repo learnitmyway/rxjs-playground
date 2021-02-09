4d26bd70644f1d9edfb678b564bcf5425d8a33d6
David <learnitmyway@users.noreply.github.com>
2021-02-09 16:53:50 +0100

    improve switchMapExample

--- src/switchMapMapExample.js
+++ src/switchMapMapExample.js
@@ -1,8 +1,8 @@
-import { of } from "rxjs";
+import { combineLatest, of } from "rxjs";
 import { pipe, switchMap, map } from "rxjs/operators";
 
 export default function switchMapMapExample() {
   of(1)
-    .pipe(switchMap((val1) => of(2).pipe(map((val2) => [val1, val2]))))
+    .pipe(switchMap((val1) => of([val1, 2])))
     .subscribe(console.log);
 }
